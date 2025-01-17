7bcca85bd2c4d8312a807f30ef6c0cb371d470b6
Sahasvinandith <sahasvinandith@gmail.com>
2025-01-17 02:32:50 -0500

    Updated code to chat continuously between

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -6,6 +7,19 @@
 #include <unistd.h>
 
 typedef struct sockaddr_in Socket_add;
+
+void string_format(char buffer[]){
+    for (int i = 0; i < sizeof(buffer); i++)
+    {
+        if(buffer[i]=='\n'){
+            return;
+        }
+        else{
+            buffer[i]=buffer[i];
+        }
+    }
+    
+}
 
 int main(){
     int client_socket=socket(AF_INET,SOCK_STREAM,0);
@@ -18,10 +32,26 @@ int main(){
     int stat=connect(client_socket,(struct sockaddr*)&server_address,sizeof(server_address));
 
     char server_response[256];
+    char client_message[256];
 
     recv(client_socket,(void*)server_response,sizeof(server_response),0);
-
     printf("Server: %s\n",server_response);
+    
+    while (server_response!="Q")
+
+    {
+        printf("client: ");
+        fgets(client_message,sizeof(client_message),stdin);
+        string_format(client_message);
+        send(client_socket,(void*)client_message,sizeof(client_message),0);
+        recv(client_socket,(void*)server_response,sizeof(server_response),0);
+        string_format(server_response);
+        printf("Server: %s\n",server_response);
+        
+    }
+    
+
+    
     close(client_socket);
     return 0;
 }
--- server.c
+++ server.c
@@ -7,9 +7,23 @@
 
 typedef struct sockaddr_in Socket_add;
 
+void string_format(char buffer[]){
+    for (int i = 0; i < sizeof(buffer); i++)
+    {
+        if(buffer[i]=='\n'){
+            return;
+        }
+        else{
+            buffer[i]=buffer[i];
+        }
+    }
+    
+}
+
 int main(){
     //create the server socket
     char server_message[256]="Hello this is the server!!";
+    char client_message[256];
 
     int server_socket=socket(AF_INET,SOCK_STREAM,0);
 
@@ -28,6 +42,20 @@ int main(){
     int client_socket=accept(server_socket,NULL,NULL);
 
     send(client_socket,(void*)server_message,sizeof(server_message),0);
+
+    while (client_message!="Q" || server_message!="Q")//develop the code to await until the client sends a reply. i guess
+    {
+        recv(client_socket,(void*)&client_message,sizeof(client_message),0);
+        string_format(client_message);
+        printf("Client: %s\n",client_message);
+        printf("Server: ");
+        fgets(server_message,sizeof(server_message),stdin);
+        string_format(server_message);
+        send(client_socket,(void*)server_message,sizeof(server_message),0);
+    }
+    
+
+    
     close(server_socket);
     return 0;
 
